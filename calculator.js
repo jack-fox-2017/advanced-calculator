bff7bb01dbceb418f0bbc8412578162cdef90a8e
jainal arifin <jainalarifin@jainals-MacBook-Pro.local>
2017-07-27 19:43:16 +0700

    advanced-calculaktor jainal arifin

--- calculator.js
+++ calculator.js
@@ -2,21 +2,43 @@
 
 class Calculator {
   //write your code here
-  constructor () {
+  constructor ( number ) {
+    this.number = number;
   }
-  add () {
+  add (num) {
+    this.number += num;
+    return this;
   }
-  substract () {
+  substract (num) {
+    this.number -= num;
+    return this
   }
-  multiply () {
+  multiply (num) {
+    this.number *= num;
+    return this
   }
-  divide () {
+  divide (num) {
+    this.number /= num;
+    return this
   }
-  square () {
+  square (num) {
+    this.number = Math.pow(this.number, num);
+    return this
   }
-  squareRoot () {
+  squareRoot (num ) {
+    this.number = Math.sqrt(this.number, num);
+    return this
+  }
+  piLingkaran(){
+    this.number = Math.PI*this.number;
+    return this
   }
 }
+
+let perhitunganCalculator = new Calculator(10);
+console.log(perhitunganCalculator.add(5).substract(5).multiply(10).divide(2).square(2).squareRoot(2).piLingkaran());
+
+
 
 /** note : you can use several features from ecmascript, such as:
 * - Classes
