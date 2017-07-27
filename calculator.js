2227346f29b386d373db0962059426f5935e6bbb
achimbaggins <achim_baggins@yahoo.com>
2017-07-27 16:50:51 +0700

    oop ugly

--- calculator.js
+++ calculator.js
@@ -2,21 +2,43 @@
 
 class Calculator {
   //write your code here
-  constructor () {
+  constructor (Num) {
+    this.Num = Num
+    // this.count = 0
   }
-  add () {
+  add (num) {
+    this.Num += num
+    return this
   }
-  substract () {
+  substract (num) {
+    this.Num -= num
+    return this
   }
-  multiply () {
+  multiply (num) {
+    this.Num *= num
+    return this
   }
-  divide () {
+  divide (num) {
+    this.Num /= num
+    return this
   }
-  square () {
+  square (num) {
+    this.Num = Math.pow(this.Num, num)
+    return this
   }
   squareRoot () {
+    this.Num = Math.sqrt(this.Num)
+    return this
+  }
+  luasLingkaran(num){
+    const Pi = 3.14
+    this.Num = Pi * Math.pow(num,2)
+    return this
   }
 }
+let count = new Calculator(2)
+// console.log(count.add(4));
+console.log(count.add(4).substract(2).multiply(4).divide(2).square(2).squareRoot().luasLingkaran(3));
 
 /** note : you can use several features from ecmascript, such as:
 * - Classes
