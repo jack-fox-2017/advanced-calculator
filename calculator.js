dce634c87724d6155c849e54f8a5f2f23efcd283
anto <irianto223@gmail.com>
2017-07-27 15:36:54 +0700

    method chaining done, luas lingkaran done

--- calculator.js
+++ calculator.js
@@ -2,19 +2,58 @@
 
 class Calculator {
   //write your code here
-  constructor () {
+  constructor (num) {
+    this.num = num
+    this.Pi = 22/7
   }
-  add () {
+
+  add (num) {
+    this.num += num
+    return this
   }
-  substract () {
+
+  substract (num) {
+    this.num -= num
+    return this
   }
-  multiply () {
+
+  multiply (num) {
+    this.num *= num
+    return this
   }
-  divide () {
+
+  divide (num) {
+    this.num /= num
+    return this
   }
-  square () {
+
+  square (num) {
+    let temp = this.num
+
+    if (num == 0) {
+      this.num = 1
+    }
+    else if (num > 1) {
+      for (let i=0; i<num-1; i++) {
+        this.num *= temp
+      }
+    }
+
+    return this
   }
+
   squareRoot () {
+    this.num = Math.sqrt(this.num)
+    return this
+  }
+
+  luasLingkaran(jari) {
+    // return new Calculator(jari).square(2).multiply(this.Pi).log()
+    return new Calculator(jari).multiply(jari).multiply(this.Pi).log()
+  }
+
+  log() {
+    return console.log(`Hasil hitung: ${this.num}`);
   }
 }
 
--- main.js
+++ main.js
@@ -3,3 +3,6 @@
 const {Calculator} = require("./calculator.js")
 
 // execute function on calculator.js in here
+var calc1 = new Calculator(16)
+calc1.add(5).substract(9).multiply(500).divide(2).log()
+calc1.luasLingkaran(21)
