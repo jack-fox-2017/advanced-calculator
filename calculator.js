63601d29b32382c1685cf96c5f082783926b1e6c
anggraito <anggraito.laras@gmail.com>
2017-07-27 20:16:51 +0700

    push calculator method

--- calculator.js
+++ calculator.js
@@ -2,22 +2,48 @@
 
 class Calculator {
   //write your code here
-  constructor () {
+  constructor (num) {
+    this._num = num
   }
-  add () {
+  add (num) {
+    this._num += num;
+    return this
   }
-  substract () {
+  substract (num) {
+    this._num -= num
+    return this
   }
-  multiply () {
+  multiply (num) {
+    this._num *= num
+    return this
   }
-  divide () {
+  divide (num) {
+    this._num /= num
+    return this
   }
-  square () {
+  square (num = 2) { //
+    this._num = Math.pow(this._num , num) //pangkat
+    return this
   }
-  squareRoot () {
+  squareRoot (num = 2) {
+    this._num = Math.pow(this._num, 1/num) //akar dari hasil
+    return this
+  }
+  roundCircle(num = 3.14){
+
+    this._num = Math.pow(this._num, 2) * num //this process num proses di kali 2 ) * phi nya
+    return this
+  }
+  result(){
+    console.log(this._num);
+    return this
   }
 }
 
+var angka = new Calculator(9)
+
+//console.log(angka.add().result());
+console.log(angka.squareRoot(2).result());
 /** note : you can use several features from ecmascript, such as:
 * - Classes
 * - Default Parameters
